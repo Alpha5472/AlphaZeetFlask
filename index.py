5c7c95e4d2d5f0eb0ec44a23d6c4f8aa230b5232
Alpha5472 <66547955+Alpha5472@users.noreply.github.com>
2023-08-31 13:51:50 +0400

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask, request, jsonify
+import subprocess
 
-app=Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('index.html')
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    app.run()#(debug=False,host='0.0.0.0')
+@app.route('/run-python-script', methods=['POST'])
+def run_python_script():
+    try:
+        # Run the Python script and capture its output
+        result = subprocess.check_output(['python', 'https://drive.google.com/file/d/1i523pd4YRI2X0fsTzYE1pXDifb2BoQs3/view?usp=sharing'], universal_newlines=True, stderr=subprocess.STDOUT)
+        return jsonify({"result": result})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
